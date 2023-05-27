7bab090db2e99da7d2bf272fb8734d1caac3185f
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-05-27 08:12:01 +0200

    first experiment for compiling linking a node against parrot arsdk

--- src/bebop_driver_node.cpp
+++ src/bebop_driver_node.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 
+#include "ros2_bebop_driver/bebop.hpp"
+
 int main(int argc, char** argv) {
     printf("hello world ros2_bebop package\n");
+    auto bebop = new bebop_driver::Bebop();
     return 0;
 }
