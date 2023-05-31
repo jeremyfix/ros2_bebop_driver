42b0b559f5ac5a7ff8e6a5688200521165bddf4b
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-05-31 14:26:23 +0200

    fix xml url

--- cmake/generate_from_xml.cmake
+++ cmake/generate_from_xml.cmake
@@ -1,9 +1,9 @@
 include(FetchContent)
 
 function(generate_states xml_filename)
-	message("Generating states from https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/${ARSDKXML_HASH}/xml/${xml_filename}")	
 
-	set(XML_URL https://raw.githubusercontent.com/Parrot-Developers/${ARSDK_HASH}/xml/${xml_filename})
+    set(XML_URL https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/${ARSDKXML_HASH}/xml/${xml_filename})
+    message("Generating states from ${XML_URL}")	
 
 	# Get the xml file
 	file(DOWNLOAD ${XML_URL} ${xml_filename})
@@ -14,9 +14,8 @@ function(generate_states xml_filename)
 endfunction()
 
 function(generate_settings xml_filename)
-	message("Generating settings from https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/${ARSDKXML_HASH}/xml/${xml_filename}")	
-
-	set(XML_URL https://raw.githubusercontent.com/Parrot-Developers/${ARSDK_HASH}/xml/${xml_filename})
+    set(XML_URL https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/${ARSDKXML_HASH}/xml/${xml_filename})
+    message("Generating states from ${XML_URL}")	
 
 	# Get the xml file
 	file(DOWNLOAD ${XML_URL} ${xml_filename})
