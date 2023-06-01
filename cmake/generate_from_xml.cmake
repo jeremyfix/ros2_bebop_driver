d88cec4e48097f4ad9a7f011f63c05ebe5636258
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-06-01 18:17:53 +0200

    fix debug comment

--- cmake/generate_from_xml.cmake
+++ cmake/generate_from_xml.cmake
@@ -14,7 +14,7 @@ endfunction()
 
 function(generate_settings xml_filename)
     set(XML_URL https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/${ARSDKXML_HASH}/xml/${xml_filename})
-    message("Generating states from ${XML_URL}")	
+    message("Generating settings from ${XML_URL}")	
 
 	# Get the xml file
 	file(DOWNLOAD ${XML_URL} ${xml_filename})
