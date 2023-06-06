c23b72f997ef6f3429dc5e23fc6f35656065422b
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-06-06 09:51:31 +0200

    base code for building and running the bebop node

--- src/bebop_driver_node.cpp
+++ src/bebop_driver_node.cpp
@@ -1,9 +1,16 @@
+#include "ros2_bebop_driver/bebop_driver_node.hpp"
+
 #include <cstdio>
 
-#include "ros2_bebop_driver/bebop.hpp"
+namespace bebop_driver {
+BebopDriverNode::BebopDriverNode()
+    : rclcpp::Node("bebop_driver_node"), bebop(std::make_shared<Bebop>()) {}
+
+}  // namespace bebop_driver
 
 int main(int argc, char** argv) {
-    printf("hello world ros2_bebop package\n");
-    auto bebop = new bebop_driver::Bebop();
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<bebop_driver::BebopDriverNode>());
+    rclcpp::shutdown();
     return 0;
 }
